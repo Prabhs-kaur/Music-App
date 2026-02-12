c984e95b8b3e70bb1c9130401a88283afb19ab58
Prabhjot Kaur <HeyPrabh@AD-MacBook-Pro.local>
2026-02-11 21:26:39 -0800

    Add Spotify API

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import "../styles/LoginPage.css";
+import axios from "axios";
+
+const USER_AUTH = {
+   "grant_type":"client_credentials",
+   
+    "Client_ID": "8800212800004724b8aed8dc5f4b2fca" ,
+    "Client_Secret" : "4b0dc981123d4e5fa6f963978c12ae6a",
+
+};
+
+const LOGIN_API = "https://accounts.spotify.com/api/token"
 
 const Login  =()=>{
+
+
+    const handleLogin = async() =>{
+     let res = await axios.post(LOGIN_API, USER_AUTH, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        })
+console.log(res);
+    }
+
+
+
     return (
        <div className="login-page">
         <div className="login-content">
-            <div className="login-button">
+            <div className="login-button" onClick={handleLogin} >
                 Login to your Spotify!
             </div>
         </div>
